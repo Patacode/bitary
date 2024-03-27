bb67f319b2b5ee7b07f7112e15132d9ed3217fc5
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-27 20:54:36 +0100

    test(bitary_handler): add unit tests for bit handler class

---
+++ spec/bitary/handler_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'bitary/handler'
+
+RSpec.describe Bitary::Handler do
+  describe '::new' do
+    it 'returns a new Handler instance with provided integer value' do
+      expect(Bitary::Handler.new(10)).to be_instance_of(Bitary::Handler)
+    end
+
+    it 'raises an ArgumentError if provided value is not an integer' do
+      expect { Bitary::Handler.new('hello') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#execute' do
+    it 'raises a NotImplementedError when called' do
+      handler = Bitary::Handler.new(100)
+
+      expect { handler.execute }.to raise_error(NotImplementedError)
+    end
+
+    it 'cannot be called with positional arguments' do
+      handler = Bitary::Handler.new(100)
+
+      expect { handler.execute(1) }.to raise_error(ArgumentError)
+    end
+  end
+end
