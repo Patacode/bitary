4c9470b9c3d2ef19a643e05284cd1f30adbaf39a
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-27 18:34:57 +0100

    test(bitary): remove surrounding example groups for constant expectations

--- spec/bitary/size_spec.rb
+++ spec/bitary/size_spec.rb
@@ -3,27 +3,19 @@
 require 'bitary/size'
 
 RSpec.describe Bitary::Size do
-  describe '::BYTE' do
-    it 'defines a size of 8 bits' do
-      expect(Bitary::Size::BYTE).to eq(8)
-    end
+  it 'defines a size of 8 bits' do
+    expect(Bitary::Size::BYTE).to eq(8)
   end
 
-  describe '::SHORT' do
-    it 'defines a size of 16 bits' do
-      expect(Bitary::Size::SHORT).to eq(16)
-    end
+  it 'defines a size of 16 bits' do
+    expect(Bitary::Size::SHORT).to eq(16)
   end
 
-  describe '::INT' do
-    it 'defines a size of 32 bits' do
-      expect(Bitary::Size::INT).to eq(32)
-    end
+  it 'defines a size of 32 bits' do
+    expect(Bitary::Size::INT).to eq(32)
   end
 
-  describe '::LONG' do
-    it 'defines a size of 64 bits' do
-      expect(Bitary::Size::LONG).to eq(64)
-    end
+  it 'defines a size of 64 bits' do
+    expect(Bitary::Size::LONG).to eq(64)
   end
 end
--- spec/bitary/version_spec.rb
+++ spec/bitary/version_spec.rb
@@ -3,9 +3,7 @@
 require 'bitary/version'
 
 RSpec.describe Bitary do
-  describe '::VERSION' do
-    it 'has a version number' do
-      expect(Bitary::VERSION).not_to be nil
-    end
+  it 'has a version number' do
+    expect(Bitary::VERSION).not_to be nil
   end
 end
