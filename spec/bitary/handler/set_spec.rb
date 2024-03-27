6100650220b2765723c29bccbeac603d0bed7a82
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-27 21:26:37 +0100

    test(bitary_handler_set): add unit tests for strong error handling

--- spec/bitary/handler/set_spec.rb
+++ spec/bitary/handler/set_spec.rb
@@ -16,5 +16,27 @@ RSpec.describe Bitary::Handler::Set do
       expect { bit_setter.execute(index: -1) }.to raise_error(IndexError)
       expect { bit_setter.execute(index: 8) }.to raise_error(IndexError)
     end
+
+    it 'raises an ArgumentError if provided index is not an integer' do
+      bit_setter = Bitary::Handler::Set.new(128)
+
+      expect { bit_setter.execute(index: 'hello') }.to raise_error(
+        ArgumentError
+      )
+    end
+
+    it 'raises a KeyError if no index kwarg is provided' do
+      bit_setter = Bitary::Handler::Set.new(128)
+
+      expect { bit_setter.execute }.to raise_error(KeyError)
+    end
+
+    it 'raises an ArgumentError if other kwargs than index are provided' do
+      bit_setter = Bitary::Handler::Set.new(128)
+
+      expect do
+        bit_setter.execute(index: 3, unexpected: 'value')
+      end.to raise_error(ArgumentError)
+    end
   end
 end
