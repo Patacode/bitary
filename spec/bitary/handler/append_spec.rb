f3d7f3ca6fe888d82d63a25bb5d5f4258a308a9d
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-29 00:23:11 +0100

    test(bitary_handler_append): update unit tests

--- spec/bitary/handler/append_spec.rb
+++ spec/bitary/handler/append_spec.rb
@@ -9,24 +9,35 @@ RSpec.describe Bitary::Handler::Append do
     end
 
     it 'gets the bit at given index (starting from leftmost bit)' do
-      expect(@bit_appender.execute(value: 0b1111)).to eq(2_063)
+      expect(@bit_appender.execute(offset: 4, value: 0b1111)).to eq(2_063)
+    end
+
+    it 'raises a KeyError if no offset kwarg is provided' do
+      expect { @bit_appender.execute(value: 0b1111) }.to raise_error(KeyError)
     end
 
     it 'raises a KeyError if no value kwarg is provided' do
-      expect { @bit_appender.execute }.to raise_error(KeyError)
+      expect { @bit_appender.execute(offset: 4) }.to raise_error(KeyError)
     end
 
     it 'raises an ArgumentError if provided value is not an integer' do
-      expect { @bit_appender.execute(value: 'hello') }.to raise_error(
+      expect { @bit_appender.execute(offset: 4, value: 'hello') }.to raise_error(
         ArgumentError
       )
     end
 
+    it 'raises an ArgumentError if provided offset is not an integer' do
+      expect do
+        @bit_appender.execute(offset: 'hello', value: 0b1111)
+      end.to raise_error(ArgumentError)
+    end
+
     it(
-      'raises an ArgumentError if other kwargs than value is provided'
+      'raises an ArgumentError if other kwargs than value or offset are ' \
+      'provided'
     ) do
       expect do
-        @bit_appender.execute(value: 0b1111, unexpected: 'value')
+        @bit_appender.execute(offset: 4, value: 0b1111, unexpected: 'value')
       end.to raise_error(ArgumentError)
     end
   end
