60abc21a65c225e64f486198d4b26c93b9413041
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-29 00:23:47 +0100

    fix(bitary_handler_append): add offset kwarg

--- lib/bitary/handler/append.rb
+++ lib/bitary/handler/append.rb
@@ -5,14 +5,15 @@ class Bitary
     class Append < Bitary::Handler
       def execute(**kwargs)
         raise ArgumentError unless kwargs.all? do |key, _value|
-          %i[value].include?(key)
+          %i[offset value].include?(key)
         end
 
+        offset = kwargs[:offset] or raise KeyError
         value = kwargs[:value] or raise KeyError
+        raise ArgumentError unless offset.is_a?(Integer)
         raise ArgumentError unless value.is_a?(Integer)
 
-        bits = value.bit_length
-        (@value << bits) | value
+        (@value << offset) | value
       end
     end
   end
