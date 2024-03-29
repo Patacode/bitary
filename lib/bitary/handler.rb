23a15b0692f2fb0118a86ec38ff2b80c6802115b
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-29 03:53:23 +0100

    fix(bitary_handler): add missing require stmts

--- lib/bitary/handler.rb
+++ lib/bitary/handler.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'handler/set'
+require_relative 'handler/unset'
+require_relative 'handler/get'
+require_relative 'handler/append'
 
 class Bitary
   class Handler
