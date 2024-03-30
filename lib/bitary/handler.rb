fd253c1c008482258fb085fe7216ddb11a95f72a
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-30 21:53:13 +0100

    feat(handler): add decorator to handle kwargs validation

--- lib/bitary/handler.rb
+++ lib/bitary/handler.rb
@@ -7,7 +7,13 @@ require_relative 'handler/append'
 
 class Bitary
   class Handler
+    SPEC = {}.freeze
+
     attr_reader :value
+
+    def self.new(*arg, **kwargs)
+      Decorator::KwargsValidator.new(super, { execute: self::SPEC })
+    end
 
     def initialize(value)
       raise ArgumentError unless value.is_a?(Integer)
