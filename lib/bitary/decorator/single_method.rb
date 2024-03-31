df0fa26d152229eebf9a4e1bc3cfb6422c7ac1a9
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-31 12:54:59 +0200

    feat(single_method): add single method deco adapter

---
+++ lib/bitary/decorator/single_method.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Bitary
+  class Decorator
+    class SingleMethod < Bitary::Decorator
+      def initialize(wrappee, method)
+        super(wrappee) { |meth| meth == method }
+        check_method(wrappee, method)
+      end
+
+      private
+
+      def check_method(wrappee, method)
+        raise ArgumentError unless method.is_a?(Symbol)
+        raise NoMethodError unless wrappee.respond_to?(method)
+      end
+    end
+  end
+end
+
+require_relative 'single_method/non_nil_enforcer'
+require_relative 'single_method/truthy_enforcer'
