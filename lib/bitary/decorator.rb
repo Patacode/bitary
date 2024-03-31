5a2554082646323d5dab3e043438f7d0232580db
Maximilien Ballesteros <maxou.info@gmail.com>
2024-03-31 12:55:32 +0200

    fix(decorator): adjust require stmts

--- lib/bitary/decorator.rb
+++ lib/bitary/decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'decorator/kwargs_validator'
-require_relative 'decorator/non_nil_enforcer'
+require_relative 'decorator/single_method'
 
 class Bitary
   class Decorator
